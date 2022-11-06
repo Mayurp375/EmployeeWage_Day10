b6db4d41c85c861d72bdb04cb36d97deab0be8be
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-11-06 23:28:58 +0530

    maain method of employee Multy company

---
+++ main.java
@@ -0,0 +1,10 @@
+package emplyoyee_Multy;
+
+public class main {
+    public static void main(String[] args) {
+       cumputeWage obj = new cumputeWage("virchow",22,25,30);
+       obj.toPrint();
+       cumputeWage obj2 = new cumputeWage("pagariya",23,27,30);
+       obj2.toPrint();
+    }
+}
